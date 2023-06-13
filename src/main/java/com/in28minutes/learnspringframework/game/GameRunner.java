c61f9533370f9c36a05387015bc57f58445fec7c
ameeraazli <wanazliwanameera@gmail.com>
2023-06-13 10:44:39 +0800

    getting started with java gaming application - tightly coupled way - adding another game to show the inflexibility of the tightly coupled way

--- src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
+++ src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
@@ -1,11 +1,15 @@
 package com.in28minutes.learnspringframework.game;
 
 public class GameRunner {
-   MarioGame game;
+//   MarioGame game;
+    private SuperContraGame game;
 
-   public GameRunner(MarioGame game) {
-       this.game = game;
-   }
+//   public GameRunner(MarioGame game) {
+//       this.game = game;
+//   }
+    public GameRunner(SuperContraGame game) {
+        this.game = game;
+    }
 
     public void run() {
        System.out.println("Running game: " + game);
