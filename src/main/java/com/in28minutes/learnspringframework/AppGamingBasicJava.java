41c6dbcf3e8d251049a110445ea60092da40f080
ameeraazli <wanazliwanameera@gmail.com>
2023-06-13 11:28:44 +0800

    using Java Interface to Make App Loosely Coupled

--- src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -7,10 +7,9 @@ import com.in28minutes.learnspringframework.game.SuperContraGame;
 public class AppGamingBasicJava {
     public static void main(String[] args) {
 
-        //var marioGame = new MarioGame();
-        var superContraGame = new SuperContraGame();
-        //var gameRunner = new GameRunner(marioGame);
-        var gameRunner = new GameRunner(superContraGame);
+        var game = new MarioGame();
+        //var game = new SuperContraGame();
+        var gameRunner = new GameRunner(game);
         gameRunner.run();
     }
 }
