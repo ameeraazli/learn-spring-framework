ea17515ea7ac33298d2ccf72d8043a1beaf8e716
ameeraazli <wanazliwanameera@gmail.com>
2023-06-13 14:02:44 +0800

    adding in spring configuration class and its beans

---
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -0,0 +1,18 @@
+package com.in28minutes.learnspringframework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App02HelloWorldSpring { //the JVM
+    public static void main(String[] args) {
+        //1: Launch a Spring Context
+        var context =
+                new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);  //AnnotationConfigApplicationContext - the Spring context
+
+        //2: Configure the things that we want Spring to manage -
+        // HelloWorldConfiguration - @Configuration class
+        //name method - @Bean
+
+        //3: Retrieving Beans managed by Spring
+        System.out.println(context.getBean("name"));
+    }
+}
